c5c4afd735a10b8abf1656279852f145616aa0af
mpoweredo <mpow3r1337@gmail.com>
2022-08-17 20:22:14 +0200

    Changed from hardcoded colors to auto chaning depending on the theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-
-import JobCard from './components/JobCard/JobCard'
 import Header from './components/Header/Header';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './global.css';
--- src/components/JobCard/JobCard.styled.tsx
+++ src/components/JobCard/JobCard.styled.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const StyledContainer = styled.div`
 	position: relative;
-	background-color: #19202d;
+	background-color: ${({ theme }) => theme.boxBackground};
 	border-radius: 6px;
 	width: 320px;
 	height: 228px;
@@ -14,12 +14,12 @@ export const StyledContainer = styled.div`
 	@media screen and (min-width: 425px) {
 		width: 340px;
 		padding: 48px 28px 32px 28px;
-	};
+	}
 
 	@media screen and (min-width: 1024px) {
 		width: 350px;
 		padding: 48px 30px 32px 30px;
-	};
+	} ;
 `;
 
 export const StyledLogoContainer = styled.div<{ bgColor: string }>`
@@ -49,7 +49,7 @@ export const StyledInfo = styled.div`
 `;
 
 export const StyledTitle = styled.h1`
-	color: white;
+	color: ${({ theme }) => theme.headingText};
 	font-weight: bolder;
 	font-size: 20px;
 	text-align: left;
@@ -60,7 +60,7 @@ export const StyledDetails = styled.div`
 	gap: 8px;
 	font-size: 16px;
 	font-weight: 400;
-	color: #6e8098;
+	color: ${({ theme }) => theme.regularText};
 `;
 
 export const StyledDot = styled.div<{ contract: string }>`
@@ -69,17 +69,17 @@ export const StyledDot = styled.div<{ contract: string }>`
 	height: 4px;
 	border-radius: 50%;
 	background: #6e8098;
-	top: ${props => props.contract === 'Full Time' ? '50%' : '64%'}
+	top: ${props => (props.contract === 'Full Time' ? '50%' : '64%')};
 `;
 
 export const StyledCompanyName = styled.p`
 	font-size: 16px;
 	font-weight: 400;
-	color: #6e8098;
-`;
-
-export const StyledLocation = styled.p`
+	color: ${({ theme }) => theme.regularText};
+	`;
+	
+	export const StyledLocation = styled.p`
 	margin-top: auto;
 	font-weight: 600;
-	color: #5964e0;
+	color: ${({ theme }) => theme.themeColor};
 `;
